7c2a4d30758e84856b7ba74d473bf24b3f432d05
Manrique3 <72946741+Manrique3@users.noreply.github.com>
2022-06-13 23:26:45 -0400

    Entidades_Sencillas
    Probando_entidades

---
+++ ProyectoDesarrolloSoftware/ProyectoDesarrolloSoftware/Entidades/Danos_terceros.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoDesarrolloSoftware.Entidades
+{
+    public class Danos_terceros
+    {
+    }
+}
