d4441570bdd652a641ec11e2393b4de5897383fd
Manrique3 <72946741+Manrique3@users.noreply.github.com>
2022-08-06 21:42:41 -0400

    Fabrica_Marca_Comppleta

---
+++ ProyectoDesarrolloSoftware/ProyectoDesarrolloSoftware/Fabricas/MarcaFabricacs.cs
@@ -0,0 +1,21 @@
+﻿using ProyectoDesarrolloSoftware.DataBase.DAOs.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoDesarrolloSoftware.Fabricas
+{
+    public class MarcaFabricacs
+    {
+        public static MarcaDAO CreateMarcaDAO(){
+
+            return new MarcaDAO();
+
+        }
+
+
+
+
+    }
+}
