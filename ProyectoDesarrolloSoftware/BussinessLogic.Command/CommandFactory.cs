c030d387f6fbda9b2c771166cc99a03920c77e7f
Manrique3 <72946741+Manrique3@users.noreply.github.com>
2022-08-08 02:42:56 -0400

    Comando_Marca

--- ProyectoDesarrolloSoftware/BussinessLogic.Command/CommandFactory.cs
+++ ProyectoDesarrolloSoftware/BussinessLogic.Command/CommandFactory.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ProyectoDesarrolloSoftware.BussinessLogic.Command.Commands.Asegurado;
+using ProyectoDesarrolloSoftware.BussinessLogic.Command.Commands.MarcasC;
 using ProyectoDesarrolloSoftware.BussinessLogic.Command.Commands.Piezas;
 
 namespace ProyectoDesarrolloSoftware.BussinessLogic.Command
@@ -19,6 +20,12 @@ namespace ProyectoDesarrolloSoftware.BussinessLogic.Command
         {
             return new VerRegistrosPiezasCommand(Nombre);
         }
+
+        public static VerRegistrosMarcaCommand createVerRegistrosPiezasCommand(int id)
+        {
+            return new VerRegistrosMarcaCommand(id);
+        }
+
 
     }
 }
---
+++ ProyectoDesarrolloSoftware/BussinessLogic.Command/Commands/MarcasC/VerRegistrosMarcaCommand.cs
@@ -0,0 +1,34 @@
+﻿using ProyectoDesarrolloSoftware.BussinesLogic.DTO.DTO;
+using ProyectoDesarrolloSoftware.DataAccess.DAOs;
+using ProyectoDesarrolloSoftware.DataAccess.Fabricas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDesarrolloSoftware.BussinessLogic.Command.Commands.MarcasC
+{
+    public class VerRegistrosMarcaCommand : Command<MarcaDTO>
+    {
+        private readonly int _marca;
+        private MarcaDTO _result;
+
+        public VerRegistrosMarcaCommand(int marca)
+        {
+            _marca = marca;
+        }
+
+        public override void Execute()
+        {
+            MarcaDAO marcaDAO = MarcaFactory.CreateMarcaDB();
+
+            _result = marcaDAO.GetMarca(_marca);
+        }
+
+        public override MarcaDTO GetResult()
+        {
+            return _result;
+        }
+    }
+}
