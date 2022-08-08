41cb1a63262ae1f016721310e4af2d8b7a15d49e
Manrique3 <72946741+Manrique3@users.noreply.github.com>
2022-08-07 23:25:52 -0400

    Command_incompleto

--- ProyectoDesarrolloSoftware/DataAccess/DataBase/DSDBContext.cs
+++ ProyectoDesarrolloSoftware/DataAccess/DataBase/DSDBContext.cs
@@ -10,55 +10,35 @@ namespace ProyectoDesarrolloSoftware.DataAccess.DataBase
         {
         }
 
-        DbContext DbContext
+        public DbContext DbContext
         {
             get
             {
                 return this;
             }
         }
-        public DbSet<Vehiculo> Vehiculos { get; set; }
-        public DbSet<Marca> Marcas { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<Administrador> Administradors { get; set; } //Entidad Administrador
-        public DbSet<Poliza> Polizas { get; set; } //Entidad de Polizas
-        public DbSet<Proveedor> Proveedores { get; set; }
-        public DbSet<Taller> Tallers { get; set; }
-        public DbSet<Perito> Peritos { get; set; } // Entidad Perito
-        public DbSet<Taller_Marca> Taller_Marcas { get; set; }
-        public DbSet<Proveedor_Marca> ProvMarcas { get; set; }
-        public DbSet<Pieza> Piezas { get; set; }
-        public DbSet<Asegurado> Asegurados { get; set; }
-        public DbSet<Incidente> Incidentes { get; set; } //Entidad de Incidentes
-        public DbSet<Cotizacion> Cotizacions { get; set; } //Entidad de Cotizacion
-        public DbSet<Pieza_Proveedor> Pieza_Proveedor { get; set; }
-        public DbSet<Administrador_Cotizacion> Administrador_Cotizacion { get; set; }
-        public DbSet<Cotizacion_Proveedor> Cotizacion_Proveedor { get; set; }
-        public DbSet<Cotizacion_Taller> Cotizacion_Taller { get; set; }
-        public DbSet<Incidente_Pieza> Incidente_Pieza { get; set; }
-        public DbSet<Pedido> Pedidos { get; set; }
+        public virtual DbSet<Vehiculo> Vehiculos { get; set; }
+        public virtual DbSet<Marca> Marcas { get; set; }
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+        public virtual DbSet<Administrador> Administradors { get; set; } //Entidad Administrador
+        public virtual DbSet<Poliza> Polizas { get; set; } //Entidad de Polizas
+        public virtual DbSet<Proveedor> Proveedores { get; set; }
+        public virtual DbSet<Taller> Tallers { get; set; }
+        public virtual DbSet<Perito> Peritos { get; set; } // Entidad Perito
+        public virtual DbSet<Taller_Marca> Taller_Marcas { get; set; }
+        public virtual DbSet<Proveedor_Marca> ProvMarcas { get; set; }
+        public virtual DbSet<Pieza> Piezas { get; set; }
+        public virtual DbSet<Asegurado> Asegurados { get; set; }
+        public virtual DbSet<Incidente> Incidentes { get; set; } //Entidad de Incidentes
+        public virtual DbSet<Cotizacion> Cotizacions { get; set; } //Entidad de Cotizacion
+        public virtual DbSet<Pieza_Proveedor> Pieza_Proveedor { get; set; }
+        public virtual DbSet<Administrador_Cotizacion> Administrador_Cotizacion { get; set; }
+        public virtual DbSet<Cotizacion_Proveedor> Cotizacion_Proveedor { get; set; }
+        public virtual DbSet<Cotizacion_Taller> Cotizacion_Taller { get; set; }
+        public virtual DbSet<Incidente_Pieza> Incidente_Pieza { get; set; }
+        public virtual DbSet<Pedido> Pedidos { get; set; }
 
-        DbContext IDSDBContext.DbContext => throw new System.NotImplementedException();
-
-        DbSet<Vehiculo> IDSDBContext.Vehiculos { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Usuario> IDSDBContext.Usuarios { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Administrador> IDSDBContext.Administradors { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Poliza> IDSDBContext.Polizas { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Proveedor> IDSDBContext.Proveedores { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Taller> IDSDBContext.Tallers { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Perito> IDSDBContext.Peritos { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Taller_Marca> IDSDBContext.Taller_Marcas { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Proveedor_Marca> IDSDBContext.ProvMarcas { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Pieza> IDSDBContext.Piezas { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Asegurado> IDSDBContext.Asegurados { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Incidente> IDSDBContext.Incidentes { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Cotizacion> IDSDBContext.Cotizacions { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Pieza_Proveedor> IDSDBContext.Pieza_Proveedor { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Administrador_Cotizacion> IDSDBContext.Administrador_Cotizacion { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Cotizacion_Proveedor> IDSDBContext.Cotizacion_Proveedor { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Cotizacion_Taller> IDSDBContext.Cotizacion_Taller { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Incidente_Pieza> IDSDBContext.Incidente_Pieza { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-        DbSet<Pedido> IDSDBContext.Pedidos { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
